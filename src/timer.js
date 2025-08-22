fa37354d9bc9559114db85e9319287d1927b77bc
StefanZagarov <stefanzagarov@gmail.com>
2025-08-22 20:20:06 +0300

    Elements position tweaked; Inputs values capped

--- src/timer.js
+++ src/timer.js
@@ -21,12 +21,51 @@ timer.addEventListener('click', () => {
     timerForm.style.display = 'flex';
 });
 
+form.elements.hours.addEventListener('input', () => {
+    if (form.elements.hours.value.length > 2) {
+        form.elements.hours.value = form.elements.hours.value.slice(0, 2);
+    }
+});
+
+form.elements.minutes.addEventListener('input', () => {
+    if (form.elements.minutes.value.length > 2) {
+        form.elements.minutes.value = form.elements.minutes.value.slice(0, 2);
+    }
+
+    if (form.elements.minutes.value > 59) {
+        form.elements.minutes.value = 59;
+    }
+});
+
+form.elements.seconds.addEventListener('input', () => {
+    if (form.elements.seconds.value.length > 2) {
+        form.elements.seconds.value = form.elements.seconds.value.slice(0, 2);
+    }
+
+      if (form.elements.seconds.value > 59) {
+        form.elements.seconds.value = 59;
+    }
+});
+
+
 setTimerButton.addEventListener('click', () => {
     const timerConcat = `${form.elements.hours.value}:${form.elements.minutes.value}:${form.elements.seconds.value}`;
-    console.log(timerConcat);
+
 
     timer.textContent = timerConcat;
 
     timerForm.style.display = 'none';
     container.style.display = 'flex';
 });
+
+function validateForm(form) {
+    const hours = form.elements.hours.value;
+    const minutes = form.elements.minutes.value;
+    const seconds = form.elements.seconds.value;
+
+    if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59 || seconds < 0 || seconds > 59) {
+        return false;
+    }
+
+    return true;
+}
