acb0760f9a3be6761ec961872c4ebb1e57f6cc89
StefanZagarov <stefanzagarov@gmail.com>
2025-08-22 17:58:25 +0300

    Initial visualization construction

---
+++ src/timer.js
@@ -0,0 +1,5 @@
+const timer = document.getElementById('timer');
+const startButton = document.getElementById('start');
+const stopButton = document.getElementById('stop');
+const resetButton = document.getElementById('reset');
+
