d9496619d6fec54afc8ab453a27b13cae442db70
StefanZagarov <stefanzagarov@gmail.com>
2025-11-29 12:01:13 +0200

    Fix linux build

--- forge.config.js
+++ forge.config.js
@@ -16,7 +16,7 @@ module.exports = {
     },
     {
       name: "@electron-forge/maker-zip",
-      platforms: ["darwin"],
+      platforms: ["darwin, linux"],
     },
     {
       name: "@electron-forge/maker-deb",
